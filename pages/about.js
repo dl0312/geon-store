1db4390c0917f45200266954a4da5586f3322d15
GeonLee <leegun2003@gmail.com>
2018-10-30 16:39:17 +0900

    Shared Components

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,13 @@
 import Head from "next/head";
+import withLayout from "../lib/withLayout";
 
-export default () => (
+const About = () => (
   <div>
-    <Head>fuck</Head>
+    <Head>
+      <title>About | Geon Store</title>
+    </Head>
     <h1>This is about page</h1>
   </div>
 );
+
+export default withLayout(About);
