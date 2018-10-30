15d4e3f2d17c1ec75da5026e623d618cde04564e
GeonLee <leegun2003@gmail.com>
2018-10-30 20:47:07 +0900

    Fetching the Movie Details

--- components/PostLink.js
+++ components/PostLink.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 export default props => (
-  <Link href={`/movie?title=${props.id}`} as={`/movie/${props.id}`}>
+  <Link href={`/movie?id=${props.id}`} as={`/movie/${props.id}`}>
     <a>{props.title}</a>
   </Link>
 );
--- pages/movie.js
+++ pages/movie.js
@@ -1,19 +1,34 @@
 import Head from "next/head";
 import { withRouter } from "next/router";
+import Axios from "axios";
 
 const Movie = props => (
   <div>
     <Head>
-      <title>{props.router.query.title} | Geon Store</title>
+      <title>{props.movie.title} | Geon Store</title>
     </Head>
-    {props.title}
-    <h1>{props.router.query.title}</h1>
-    <h1>movie</h1>
+    <h1>{props.movie.title}</h1>
+    <p>{props.movie.description_intro}</p>
   </div>
 );
 
-// Movie.getInitialProps = async () => {
-//   return { title: "lalalalalala" };
-// };
+Movie.getInitialProps = async context => {
+  console.log(context);
+  const {
+    query: { id }
+  } = context;
+  const data = await Axios.get(
+    `https://yts.am/api/v2/movie_details.json?movie_id=${id}`
+  );
+  console.log(id, data);
+  const {
+    data: {
+      data: { movie }
+    }
+  } = await Axios.get(
+    `https://yts.am/api/v2/movie_details.json?movie_id=${id}`
+  );
+  return { movie };
+};
 
 export default withRouter(Movie);
