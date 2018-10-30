ad89649b52333a1e07079e2f4631c3cbbc693de8
GeonLee <leegun2003@gmail.com>
2018-10-30 16:28:19 +0900

    LInks and Headers in NextJS

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+import Head from "next/head";
+
+export default () => (
+  <div>
+    <Head>fuck</Head>
+    <h1>This is about page</h1>
+  </div>
+);
--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,11 @@
-export default () => <h1>Hello from index</h1>;
+import Head from "next/head";
+import Link from "next/link";
+
+export default () => (
+  <div>
+    <h1>Hello from index</h1>
+    <Link href={"/about"}>
+      <a>About page</a>
+    </Link>
+  </div>
+);
